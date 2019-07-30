a6ec33d4b5ec7d1a846ea33275e7f1a5a50360ec
Evonic <gevonic@gmail.com>
2019-07-30 11:48:29 +0300

    Create cfg0.lua
    123

---
+++ cfg0.lua
@@ -0,0 +1,15 @@
+cfg = {
+esp=4,
+wfssid1="wfnet1",
+wfpwd1="pass",
+wfssid2="wfnet2",
+wfcli=1,
+ip_send="ip smart house",
+timer0=1,
+send_data=1,
+period_sd=30,
+pwf=10,
+col_wifi_st=5,
+time_wf_ap=600,
+}
+ 
